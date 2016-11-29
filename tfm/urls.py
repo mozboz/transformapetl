ef4b0facb1c85525bc0cd2a179f35d195f10d0d8
Eli Gothill <egothill@gmail.com>
2016-11-29 14:07:18 +0100

    New API endpoint

--- tfm/urls.py
+++ tfm/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-import places.api
+import places.api, places.views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^places-api/', include(places.api.router.urls)),
+    url(r'^map-instance/(?P<map_instance_id>\d+)/$', places.views.map_instance_geojson)
 ]
